62d8471354c250566b206b79129628ef3afeefc9
sharwilly <yapzorenss@mail.ru>
2026-07-25 21:40:27 +0300

    Fix API URL template strings

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -64,7 +64,7 @@ L.tileLayer(
 
 
 // получаем проблемы из нашего API
-fetch('${API_URL}/problems')
+fetch(`${API_URL}/problems`)
 .then(response => response.json())
 .then(data => {
 
@@ -604,7 +604,7 @@ document
     }
 
     const response = await fetch(
-        '${API_URL}/problems',
+        `${API_URL}/problems`,
         {
             method: "POST",
             body: formData
