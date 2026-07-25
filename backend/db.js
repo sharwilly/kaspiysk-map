dbb81b4afdc0b417b38ced31bdcb09f0d9b91710
sharwilly <yapzorenss@mail.ru>
2026-07-25 21:02:01 +0300

    Connect backend to Neon PostgreSQL

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const { Pool } = require("pg");
-require("dotenv").config();
-
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
