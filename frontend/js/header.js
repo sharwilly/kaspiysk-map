9c6deb92efff00636053039af59cfbca591a7e36
sharwilly <yapzorenss@mail.ru>
2026-07-29 16:44:58 +0300

    add main menu

--- frontend/js/header.js
+++ frontend/js/header.js
@@ -2,9 +2,8 @@ fetch("components/header.html")
 .then(response => response.text())
 .then(data => {
 
-    document.body.insertAdjacentHTML(
-        "afterbegin",
-        data
-    );
+    document
+    .getElementById("header-container")
+    .innerHTML = data;
 
 });
