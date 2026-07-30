c1ac85f628ee597448376b3f8e3f0c643bdb53d8
sharwilly <yapzorenss@mail.ru>
2026-07-30 22:27:15 +0300

    asdfasdf

--- backend/telegram-test.js
+++ backend/telegram-test.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
 
@@ -13,7 +15,8 @@ const client = new TelegramClient(
     apiId,
     apiHash,
     {
-        connectionRetries: 5
+        connectionRetries: 10,
+        useWSS: true
     }
 );
 
