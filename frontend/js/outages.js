42c6f3da237bace376906ab3fea46c4553364efa
sharwilly <yapzorenss@mail.ru>
2026-08-02 20:45:49 +0300

    update outages

--- frontend/js/outages.js
+++ frontend/js/outages.js
@@ -1,43 +1,123 @@
 const API_URL = "https://kaspiysk-map-1.onrender.com";
 
 
-fetch(`${API_URL}/outages`)
-
-.then(response => response.json())
-
-.then(data => {
-
-
 const container =
 document.getElementById("outagesList");
 
 
+const buttons =
+document.querySelectorAll(".tab-button");
 
-if(data.length === 0){
+
+
+async function loadOutages(type = "active"){
+
 
     container.innerHTML =
-    "<p>Активных отключений нет</p>";
+    "Загрузка...";
 
-    return;
+
+    let url;
+
+
+    if(type === "done"){
+
+        url = ${API_URL}/outages/done;
+
+    } else {
+
+        url = ${API_URL}/outages;
+
+    }
+
+
+
+    try {
+
+
+        const response =
+        await fetch(url);
+
+
+        const data =
+        await response.json();
+
+
+
+        renderOutages(data, type);
+
+
+
+    } catch(error){
+
+
+        console.error(error);
+
+
+        container.innerHTML =
+        "<p>Ошибка загрузки данных</p>";
+
+    }
 
 }
 
 
 
-container.innerHTML = data.map(outage => `
+
+
+function renderOutages(data, type){
+
+
+    if(data.length === 0){
+
+
+        container.innerHTML =
+
+        type === "done"
+
+        ?
+        "<p>Решенных отключений нет</p>"
+
+        :
+        "<p>Активных отключений нет</p>";
+
+
+        return;
+
+    }
+
+
+
+    container.innerHTML = data.map(outage => `
+
 
 
 <div class="outage-card">
 
 
 <h2>
-⚡ Фидер-${outage.feeder}
+⚡ Фидер-${outage.feeder || "не указан"}
 </h2>
 
 
-<p class="outage-active">
-🔴 Активное отключение
+
+<p class="${type === "done" ? "outage-done" : "outage-active"}">
+
+
+${type === "done"
+
+?
+"✅ Отключение устранено"
+
+:
+
+"🔴 Активное отключение"
+
+}
+
+
 </p>
+
 
 
 <p>
@@ -45,8 +125,9 @@ ${outage.description}
 </p>
 
 
+
 <p>
-🕒 Восстановление:
+🕒 Время восстановления:
 ${outage.restore_time || "не указано"}
 </p>
 
@@ -57,21 +138,85 @@ ${outage.restore_time || "не указано"}
 </h3>
 
 
+
 <div class="address-list">
 
-${outage.addresses
-.map(address => `<div>${address}</div>`)
-.join("")}
+${
+(outage.addresses || [])
+.map(address =>
+<div>${address}</div>
+)
+.join("")
+}
 
 
 </div>
 
 
+
+${
+type === "done"
+
+?
+
+`
+<p class="date">
+🗓 Создано:
+${new Date(outage.created_at)
+.toLocaleString("ru-RU")}
+</p>
+`
+
+:
+
+""
+
+}
+
+
+
 </div>
+
 
 
 `).join("");
 
 
 
+}
+
+
+
+
+
+buttons.forEach(button=>{
+
+
+button.addEventListener("click",()=>{
+
+
+buttons.forEach(btn=>
+btn.classList.remove("active")
+);
+
+
+button.classList.add("active");
+
+
+
+loadOutages(
+button.dataset.tab
+);
+
+
+
 });
+
+
+});
+
+
+
+// первая загрузка
+
+loadOutages();
