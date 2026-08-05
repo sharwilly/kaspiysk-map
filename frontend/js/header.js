dc0876f06508c9b3cc16f105961deae443194cc7
sharwilly <yapzorenss@mail.ru>
2026-08-06 00:55:12 +0300

    added weather menu

--- frontend/js/header.js
+++ frontend/js/header.js
@@ -30,3 +30,17 @@ fetch("components/header.html")
     }
 
 });
+
+const weatherBtn = document.getElementById("weatherBtn");
+const weatherPopover = document.getElementById("weatherPopover");
+
+weatherBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    weatherPopover.classList.toggle("hidden");
+});
+
+document.addEventListener("click", (e) => {
+    if (!weatherPopover.contains(e.target) && !weatherBtn.contains(e.target)) {
+        weatherPopover.classList.add("hidden");
+    }
+});
