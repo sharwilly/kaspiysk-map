c9e9669ed39a87c9e2f7cc21043df167dac6fa9e
sharwilly <yapzorenss@mail.ru>
2026-08-19 15:20:17 +0300

    Добавить корневой Vercel endpoint для мониторинга мусоровозов

---
+++ api/trucks.js
@@ -0,0 +1,3 @@
+// Vercel endpoint для конфигураций, где Root Directory = корень репозитория.
+// Основная реализация находится в frontend/api/trucks.js.
+module.exports = require("../frontend/api/trucks.js");
